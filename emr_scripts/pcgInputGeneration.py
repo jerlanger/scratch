5ae54ade74d53715b53724aded3b29d1d55ec1c7
jerlanger <jerlanger@liventent.com>
2019-09-24 16:18:29 +0200

    updated pcgInputGeneration.py to include dummy columns, gzip and not include metadata files in s3 folder

--- emr_scripts/pcgInputGeneration.py
+++ emr_scripts/pcgInputGeneration.py
@@ -1,6 +1,7 @@
-def pcg(app,startDate,endDate=None): 
+def pcg(app,startDate=None,endDate=None): 
     from pyspark.sql import SparkSession
-    from pyspark.sql.functions import col
+    from pyspark.sql.functions import col, lit
+    from pyspark.sql.types import StringType
     from datetime import date, timedelta, datetime
     
     spark = SparkSession.builder \
@@ -11,17 +12,21 @@ def pcg(app,startDate,endDate=None):
         .getOrCreate()
     
     spark.catalog.setCurrentDatabase("default")
+    spark._jsc.hadoopConfiguration().set("mapreduce.fileoutputcommitter.marksuccessfuljobs","false")
     
     graphMaxDate = spark.sql("""SELECT MAX(date_p) FROM auto_dmps.all_features_mapping_pair""").collect()[0][0]
     
     if endDate == None:
         endDate = graphMaxDate
         
+    if startDate == None:
+        startDate = (datetime.strptime(endDate, '%Y%m%d') + timedelta(days=-6)).strftime("%Y%m%d")
+        
     start = datetime.strptime(startDate, '%Y%m%d')
     end = datetime.strptime(endDate, '%Y%m%d')
     
     baseS3Loc = "s3n://li-identity-evidence-data-production/"
-    s3Paths = []    
+    s3Paths = []
 
     delta = end - start
     for i in range(delta.days + 1):
@@ -51,10 +56,12 @@ def pcg(app,startDate,endDate=None):
     
     pcgHem = allHem.join(pcgCookies,allHem.cookie == pcgCookies.cookie) \
                         .select(allHem.hem) \
-                        .distinct()
+                        .distinct() \
+                        .withColumn("dummyCol1",lit(None).cast(StringType())) \
+                        .withColumn("dummyCol2",lit(None).cast(StringType()))
     
     pcgHemWrite = "s3n://ds-emr-storage/pcg_files/%s/%s_to_%s/" % (app, startDate, endDate)
     
-    pcgHem.write.csv(pcgHemWrite, sep="\t", mode="overwrite")
+    pcgHem.write.csv(pcgHemWrite, sep="\t", mode="overwrite", compression="gzip", header=False)
     
     snowplowReduce.unpersist()
